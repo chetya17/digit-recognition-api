da57eaff455527a9c10b7769ca085c4fe67158a2
RionDsilva7 <dsilvarion@gmail.com>
2023-03-31 17:04:00 +0530

    Fixed bugs

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ from model.model import __version__ as model_version
 
 app = FastAPI()
 
-# class TextIn(BaseModel):
-#     text: str
-
+class TextIn(BaseModel):
+    text: str
 
 class PredictionOut(BaseModel):
     feel: int
 
-# @app.get("/")
-# def home():
-#     return {model_version}
+@app.get("/")
+def home():
+    return {model_version}
 
-@app.get("/predict/{text}", response_model=PredictionOut)
-def predict(text: str):
-    feel = predict_pipeline(text)
+@app.post("/predict", response_model=PredictionOut)
+def predict(payload: TextIn):
+    feel = predict_pipeline(payload.text)
     return {"feel": feel}
