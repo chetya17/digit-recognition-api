62f8c7bb4e812d7927ef30132c41e3ba50655aa8
RionDsilva7 <dsilvarion@gmail.com>
2023-04-04 14:26:38 +0530

    modify input through string

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from model.model import predict_pipeline
 from model.model import __version__ as model_version
+import json
+from types import SimpleNamespace
 
 app = FastAPI()
 
 class TextIn(BaseModel):
     text: str
+    # x = json.loads(text, object_hook=lambda d: SimpleNamespace(**d))
+
 
 class PredictionOut(BaseModel):
     feel: int
@@ -15,7 +19,9 @@ class PredictionOut(BaseModel):
 def home():
     return {model_version}
 
-@app.post("/predict", response_model=PredictionOut)
-def predict(payload: TextIn):
-    feel = predict_pipeline(payload.text)
+@app.post("/predict/{text}", response_model=PredictionOut)
+def predict(text: str):
+    feel = predict_pipeline(text)
     return {"feel": feel}
+
+# payload: TextIn
